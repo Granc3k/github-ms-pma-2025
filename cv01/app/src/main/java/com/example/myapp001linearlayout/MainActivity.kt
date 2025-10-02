b4f444fb544aff370173f5923070bed78c4b8025
Martin Granc3k Šimon - PRDOLA <mart.simon17@gmail.com>
2025-10-02 11:40:44 +0200

    #1 Submit of cviko 1

---
+++ cv01/app/src/main/java/com/example/myapp001linearlayout/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.myapp001linearlayout
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+
+        val etName = findViewById<EditText>(R.id.etName)
+        val etSurname = findViewById<EditText>(R.id.etSurname)
+        val etAdress = findViewById<EditText>(R.id.etAdress)
+        val etAge = findViewById<EditText>(R.id.etAge)
+
+        val btnShip = findViewById<Button>(R.id.btnShip)
+        val btnDelete = findViewById<Button>(R.id.btnDelete)
+
+        val tvInformation = findViewById<TextView>(R.id.tvInformation)
+
+
+        btnShip.setOnClickListener {
+            val name = etName.text.toString();
+            val surname = etSurname.text.toString();
+            val adress = etAdress.text.toString();
+            val age = etAge.text.toString();
+
+            val output = """
+                Jméno: $name 
+                
+                ][Đđ
+                Příjmení: $surname
+                Věk: $age
+                Adresa: $adress
+            """.trimIndent()
+        }
+    }
+}
