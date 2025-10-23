0fe8b93e4ac86dc2638cdbbc16d87db0c4bcebe2
Martin Granc3k Šimon - PRDOLA <mart.simon17@gmail.com>
2025-10-23 11:38:32 +0200

    init of #9

---
+++ MyApp009aImageToApp/app/src/main/java/com/example/myapp009aimagetoapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapp009aimagetoapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapp009aimagetoapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //registerForActivityResult - Až uživatel vybere soubor (např. obrázek), vrať mi výsledek jako uri a já s ním něco udělám
+        //
+        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            binding.ivImage.setImageURI(uri)
+        }
+
+        binding.btnImage.setOnClickListener {
+            //tento příkaz zpustí výběr obrázku z telefonu
+            // Vybraný obrázek se zobrazí v ivImage
+            getContent.launch("image/*")
+        }
+    }
+}
